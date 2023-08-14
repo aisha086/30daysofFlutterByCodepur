2b86e899e05342680f28d4bcd0905858e499fa2b
aisha086 <aishasiddiqui633@gmail.com>
2023-08-14 20:06:37 +0500

    First commit

---
+++ android/app/src/main/kotlin/com/example/catalog_project_codepur/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.catalog_project_codepur
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context)
+  {
+    return const MaterialApp(
+      home: Material(
+        child: Center(
+          child: Text('30 Days of flutter'),
+        ),
+      ),
+    );
+  }
+
+}
